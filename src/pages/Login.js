8b43e532ddcd579cec24ff515c8ae1e9c7b67e93
Harvey Sutton <hsuttonjr@gmail.com>
2020-05-25 17:33:21 -0400

    user signup page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = {
+      email,
+      password,
+    };
+    console.log(formData);
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <Container className="login-page">
-      <Form className="form-login">
+      <Form className="form-login" onSubmit={handleSubmit}>
         <h4 className="logo">Neptune Projects Login</h4>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" name="email" />
+          <Form.Control
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </Form.Group>
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" name="password" />
+          <Form.Control
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </Form.Group>
         <Button variant="primary" type="submit" block>
           Login
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,29 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+const axios = require("axios");
 
 function Signup() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      firstName,
+      lastName,
+      email,
+      password,
+    };
+    axios
+      .post("http://localhost:5000/api/v1/users/signup", data)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <Container className="login-page">
-      <Form className="form-login">
+      <Form className="form-login" onSubmit={handleSubmit}>
         <h4 className="logo">Get started with Neptune Projects</h4>
         <Form.Group controlId="formFirstName">
           <Form.Label>First Name</Form.Label>
-          <Form.Control type="text" name="firstname" />
+          <Form.Control
+            type="text"
+            name="firstname"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
         </Form.Group>
         <Form.Group controlId="formLastName">
           <Form.Label>Last Name</Form.Label>
-          <Form.Control type="text" name="lastname" />
+          <Form.Control
+            type="text"
+            name="lastname"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
         </Form.Group>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" name="email" />
+          <Form.Control
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </Form.Group>
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" name="password" />
+          <Form.Control
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </Form.Group>
         <Button variant="primary" type="submit" block>
           Sign Up
